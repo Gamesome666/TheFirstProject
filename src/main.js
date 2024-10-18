080b2ed2075d0ea7e9d6ed8cac9f201b2264d888
Gamesome666 <2832156958@qq.com>
2024-10-18 20:45:33 +0800

    '初始化配置'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
+import App from './App.vue'
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
 import { createApp } from 'vue'
 import router from "./router/router.js"
-import App from './App.vue'
+
 import { createPinia } from 'pinia'
 const app=createApp(App)
+// 自动引入图标
+Object.keys(ElementPlusIconsVue).forEach((key) => {
+    app.component(key, ElementPlusIconsVue[key]);
+  });
+  
+
+
 const pinia=createPinia()
 app.use(router).use(pinia)
+app.use(ElementPlus)
 app.mount("#app")
 
--- src/pages/myHome.vue
+++ src/pages/myHome.vue
@@ -3,6 +3,8 @@ import {useUserStore} from "../store/user.js"
 const user=useUserStore();
 </script>
 <template>
-    <div>出现吧</div>
+    <div class="text-xs text-red-100">出现吧</div>
     <span>{{ user.token }}</span>
+    <el-icon><Loading /></el-icon>
+    <el-icon><Search /></el-icon>
 </template>
